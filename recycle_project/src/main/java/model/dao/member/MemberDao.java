9ec6e8c28f0be0516db7bd1dd393e9efc0e9e87a
ithodol <ddmdjesz1@gmail.com>
2025-02-10 16:46:17 +0900

    2/10 전은서 회원가입 백엔드, 최상위 Dao 작성

--- recycle_project/src/main/java/model/dao/member/MemberDao.java
+++ recycle_project/src/main/java/model/dao/member/MemberDao.java
@@ -1,5 +1,38 @@
 package model.dao.member;
 
-public class MemberDao {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import model.dao.Dao;
+import model.dto.member.MemberDto;
+
+@NoArgsConstructor( access = lombok.AccessLevel.PRIVATE )
+public class MemberDao extends Dao {
+	@Getter
+	private static MemberDao instance = new MemberDao();
+	
+	
+	// 회원가입
+	public boolean signup(MemberDto memberDto) {
+		try {
+			String sql = "insert into member(mid, mpwd, mname, mnickname, mphone, memail, mprofile) values (?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, memberDto.getMid());
+			ps.setString(2, memberDto.getMpwd());
+			ps.setString(3, memberDto.getMname());
+			ps.setString(4, memberDto.getMnickname());
+			ps.setString(5, memberDto.getMphone());
+			ps.setString(6, memberDto.getMemail());
+			ps.setString(7, memberDto.getMprofile());
+			int c = ps.executeUpdate();
+			if(c == 1) {
+				return true;
+			}
+		}catch(SQLException e) {System.out.println(e);}
+		
+		return false;
+	}
+	
 }
--- recycle_project/src/main/java/model/dto/member/MemberDto.java
+++ recycle_project/src/main/java/model/dto/member/MemberDto.java
@@ -1,5 +1,22 @@
 package model.dto.member;
 
-public class MemberDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @ToString
+public class MemberDto {
+	private int mno;
+	private String mid;
+	private String mpwd;
+	private String mname;
+	private String mnickname;
+	private String mphone;
+	private String memail;
+	private String mdate;
+	private int mpoint;
+	private String mprofile;
 }
