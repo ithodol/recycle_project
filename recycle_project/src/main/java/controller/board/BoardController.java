998c79bdbd36173c8f45b945ccffa56417918898
PHM-dung2 <pp4992@naver.com>
2025-02-12 16:37:22 +0900

    02/12 박희만 : 회원 info.jsp 상세정보 프론트엔드 샘플

--- recycle_project/src/main/java/controller/board/BoardController.java
+++ recycle_project/src/main/java/controller/board/BoardController.java
@@ -1,9 +1,42 @@
 package controller.board;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("")
+@WebServlet("/board")
 public class BoardController extends HttpServlet{
-
+	
+//	1. 게시물 쓰기
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+//	2. 전체 게시물 조회
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+//	3. 
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+	
+//	4. 
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+	
 }
