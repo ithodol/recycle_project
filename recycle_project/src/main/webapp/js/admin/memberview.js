85cfe77a1471fbc7abf0ead527830734c623da9e
ryuyena0305 <ryuyena0305@gmail.com>
2025-02-13 22:18:34 +0900

    2/13 류예나 : 회원상세조회 및 포인트로그 프론트 완료

--- recycle_project/src/main/webapp/js/admin/memberview.js
+++ recycle_project/src/main/webapp/js/admin/memberview.js
@@ -1,11 +1,46 @@
+const mno = new URL(location.href).searchParams.get('mno');
+
 const findByMno = ()=>{
-	const mno = new URL(location.href).searchParams.get('mno');
 	
 	const option={method:'GET'}
 	fetch(`/recycle_project/admin/view?mno=${mno}`,option)
 	.then(r=>r.json())
 	.then(data=>{
-		document.querySelector()
+		if(data != null){
+			const getmember = document.querySelector('.getmember');
+			let html =`<tr>
+			<td class="no"> ${data.mno} </td>
+			<td > ${data.mid} </td>
+			<td > ${data.mname} </td>
+			<td > ${data.mpoint} </td>
+			</tr>`;
+			getmember.innerHTML = html;
+		}
 	})
 }
 findByMno();
+
+const findByMnoPoint= () =>{
+	
+	const option ={method : 'GET'}
+	fetch(`/recycle_project/admin/point?mno=${mno}`,option)
+	.then(r=>r.json())
+		.then(point=>{
+			if(point != null){
+				const memberbypoint =  document.querySelector('.memberbypoint');
+				let html=``;
+				point.forEach( (point,index)=>{
+					html += `<tr>
+					     <td class="no"> ${ index+1 } </th> 
+					     <td > ${ point.point } </th>
+					     <td > ${ point.pocontent } </th> 
+					     <td > ${ point.podate } </th>
+					    </tr>`
+				})
+				memberbypoint.innerHTML = html;
+			}
+		})
+		.catch( e => { console.log(e) } )
+			
+}
+findByMnoPoint();
