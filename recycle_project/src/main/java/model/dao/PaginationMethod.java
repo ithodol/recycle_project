bc81d2d01b53057219b3ec76029d3789c26cd66c
PHM-dung2 <pp4992@naver.com>
2025-02-18 18:51:23 +0900

    2/18 박희만 : 페이지네이션 수정

--- recycle_project/src/main/java/model/dao/PaginationMethod.java
+++ recycle_project/src/main/java/model/dao/PaginationMethod.java
@@ -32,10 +32,7 @@ public class PaginationMethod implements Pagination {
             result = (ArrayList<T>) BoardDao.getInstance().findAll(startRow, display, "");
         } else if (dtoClass.equals(MemberDto.class)) {
             result = (ArrayList<T>) GetMemberDao.getInstance().findAll(startRow, display);
-        } else if (dtoClass.equals(PointLogDto.class)) { 
-        	int loginMno = pageDto.getLoginMno();
-        	result = (ArrayList<T>) MemberDao.getInstance().getPointLog(loginMno, startRow, display);
-    	} else {
+        } else {
             throw new IllegalArgumentException("Unsupported DTO type");
         }
 	    
@@ -49,30 +46,38 @@ public class PaginationMethod implements Pagination {
         return pageDto;
     } // f end
     
-//    @Override
-//    public <T extends DataDto> PageDto<T> calPagination( int page, String table, Class<T> dtoClass, String where ) {
-//	    int display = 10;
-//	    int btnSize = 10;
-//	    int startRow = (page-1) * displa y; 
-////	    조회할 테이블 튜플의 총 개수
-//	    int totalSize = BoardDao.getInstance().getTotalSize(table);
-//	    ArrayList<T> result = BoardDao.getInstance().findAll(startRow, display, where);
-//    	
-//        int totalPage = (totalSize % display == 0) ? (totalSize / display) : (totalSize / display + 1);
-//        int startBtn = ((page - 1) / btnSize) * btnSize + 1;
-//        int endBtn = startBtn + (btnSize - 1);
-//        if (endBtn > totalPage) { endBtn = totalPage; }
-//        
-//        PageDto<T> pageDto = new PageDto<>();
-//        pageDto.setTotalSize(totalSize);
-//        pageDto.setPage(page);
-//        pageDto.setTotalPage(totalPage);
-//        pageDto.setStartBtn(startBtn);
-//        pageDto.setEndBtn(endBtn);
-//        pageDto.setData(result);
-//        
-//        return pageDto;
-//    }
+    @Override 
+    public <T extends DataDto> PageDto<T> calPagination( int mno, int page, String table, Class<T> dtoClass ) {
+	    int display = 10;
+	    int btnSize = 10;
+	    int startRow = (page-1) * display; 
+//	    조회할 테이블 튜플의 총 개수
+	    int totalSize = BoardDao.getInstance().getTotalSize(table);
+	    int totalPage = (totalSize % display == 0) ? (totalSize / display) : (totalSize / display + 1);
+	    int startBtn = ((page - 1) / btnSize) * btnSize + 1;
+	    int endBtn = startBtn + (btnSize - 1);
+	    if (endBtn > totalPage) { endBtn = totalPage; }
+	    
+	    PageDto<T> pageDto = new PageDto<T>();
+	    ArrayList<T> result;
+
+	    if (dtoClass.equals(PointLogDto.class)) { 
+        	result = (ArrayList<T>) GetMemberDao.getInstance().findPointLog(mno, startRow, display);
+    	} else if (dtoClass.equals(PointDto.class)) { 
+        	result = (ArrayList<T>) MemberDao.getInstance().getPointLog(mno, startRow, display);
+    	} else {
+            throw new IllegalArgumentException("Unsupported DTO type");
+        }
+	    
+        pageDto.setTotalSize(totalSize);
+        pageDto.setPage(page);
+        pageDto.setTotalPage(totalPage);
+        pageDto.setStartBtn(startBtn);
+        pageDto.setEndBtn(endBtn);
+        pageDto.setData(result);
+        
+        return pageDto;
+    } // f end
     
 
 	  
