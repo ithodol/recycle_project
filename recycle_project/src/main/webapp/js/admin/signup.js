daccb86cfe859c103b89b415e2c17b9a9d8704cd
ryuyena0305 <ryuyena0305@gmail.com>
2025-02-20 14:19:13 +0900

    2/20 류예나 : 최종 완료 DB 댓글 샘플 추가

--- recycle_project/src/main/webapp/js/admin/signup.js
+++ recycle_project/src/main/webapp/js/admin/signup.js
@@ -1,98 +1,97 @@
 const onSignUp = () => {
-    const adid = document.querySelector('.adidinput').value;
-    const adpwd = document.querySelector('.adpwdinput').value;
-    const adname = document.querySelector('.adnameinput').value;
-    const adnickname = document.querySelector('.adnicknameinput').value;
-    const adphone = document.querySelector('.adphoneinput').value;
-    const ademail = document.querySelector('.ademailinput').value;
+	const adid = document.querySelector('.adidinput').value;
+	const adpwd = document.querySelector('.adpwdinput').value;
+	const adname = document.querySelector('.adnameinput').value;
+	const adnickname = document.querySelector('.adnicknameinput').value;
+	const adphone = document.querySelector('.adphoneinput').value;
+	const ademail = document.querySelector('.ademailinput').value;
 
-    if (adid === "") {
-        alert('ID를 입력해주세요.');
-        return;
-    }
-    if (adpwd === "") {
-        alert('PW를 입력해주세요.');
-        return;
-    }
-    if (adname === "") {
-        alert('이름을 입력해주세요.');
-        return;
-    }
-    if (adnickname === "") {
-        alert('닉네임을 입력해주세요.');
-        return;
-    }
-    if (adphone === "") {
-        alert('연락처를 입력해주세요.');
-        return;
-    }
-    if (ademail === "") {
-        alert('이메일을 입력해주세요.');
-        return;
-    }
+	if (adid === "") {
+		alert('ID를 입력해주세요.');
+		return;
+	}
+	if (adpwd === "") {
+		alert('PW를 입력해주세요.');
+		return;
+	}
+	if (adname === "") {
+		alert('이름을 입력해주세요.');
+		return;
+	}
+	if (adnickname === "") {
+		alert('닉네임을 입력해주세요.');
+		return;
+	}
+	
+	if (adphone === "") {
+		alert('연락처를 입력해주세요.');
+		return;
+	} else if (!/^\d{3}-\d{4}-\d{4}$/.test(adphone)) {
+		alert('올바른 형식으로 연락처를 입력해주세요.');
+		return;
+	}
 
-    if (!/^\d{3}-\d{4}-\d{4}$/.test(adphone)) {
-        alert('올바른 형식으로 연락처를 입력해주세요.');
-        return;
-    }
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(ademail)) {
-        alert('올바른 형식으로 이메일을 입력해주세요.');
-        return;
-    }
+	if (ademail === "") {
+		alert('이메일을 입력해주세요.');
+		return;
+	} else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(ademail)) {
+		alert('올바른 형식으로 이메일을 입력해주세요.');
+		return;
+	}
 
-    fetch('/recycle_project/admin/signup', { method: 'GET' })
-        .then(response => response.json())
-        .then(check => {
-            let isDuplicate = false;  // 중복 여부 확인 변수
+	fetch('/recycle_project/admin/signup', { method: 'GET' })
+		.then(response => response.json())
+		.then(check => {
+			let isDuplicate = false;  // 중복 여부 확인 변수
 
-            check.forEach(data => {
-                if (data.adid === adid) {
-                    alert('존재하는 ID입니다.');
-                    document.querySelector('.adidinput').value = '';
-                    isDuplicate = true;
-                }
-                if (data.adphone === adphone) {
-                    alert('존재하는 연락처입니다.');
-                    document.querySelector('.adphoneinput').value = '';
-                    isDuplicate = true;
-                }
-                if (data.ademail === ademail) {
-                    alert('존재하는 이메일입니다.');
-                    document.querySelector('.ademailinput').value = '';
-                    isDuplicate = true;
-                }
-            });
+			check.forEach(data => {
+				if (data.adid === adid) {
+					alert('존재하는 ID입니다.');
+					document.querySelector('.adidinput').value = '';
+					isDuplicate = true;
+				}
+				if (data.adphone === adphone) {
+					alert('존재하는 연락처입니다.');
+					document.querySelector('.adphoneinput').value = '';
+					isDuplicate = true;
+				}
+				if (data.ademail === ademail) {
+					alert('존재하는 이메일입니다.');
+					document.querySelector('.ademailinput').value = '';
+					isDuplicate = true;
+				}
+			});
 
-            // 중복이 있을 경우 회원가입을 진행하지 않음
-            if (isDuplicate) {
-                return;  // 이후 코드를 실행하지 않도록 함
-            }
+			// 중복이 있을 경우 회원가입을 진행하지 않음
+			if (isDuplicate) {
+				return;  // 이후 코드를 실행하지 않도록 함
+			}
 
-            const obj = {
-                adid: adid,
-                adpwd: adpwd,
-                adname: adname,
-                adnickname: adnickname,
-                adphone: adphone,
-                ademail: ademail
-            };
+			const obj = {
+				adid: adid,
+				adpwd: adpwd,
+				adname: adname,
+				adnickname: adnickname,
+				adphone: adphone,
+				ademail: ademail
+			};
 
-            return fetch('/recycle_project/admin/signup', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(obj)
-            });
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data === true) {
-                alert('회원가입 성공');
-                location.href = 'login.jsp';
-            } else {
-                alert('회원가입 실패');
-            }
-        })
-        .catch(error => {
-            console.error('에러 발생:', error);
-        });
+			return fetch('/recycle_project/admin/signup', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(obj)
+			});
+		})
+		.then(response => response.json())
+		.then(data => {
+			if (data === true) {
+				alert('회원가입 성공');
+				location.href = 'login.jsp';
+			} else {
+				alert('회원가입 실패');
+			}
+		})
+		.catch(error => {
+			console.error('에러 발생:', error);
+		});
 };
--- recycle_project/src/main/webapp/jsp/member/header.jsp
+++ recycle_project/src/main/webapp/jsp/member/header.jsp
@@ -39,7 +39,7 @@
 					<li class="nav-item"><a class="header_menu nav-link mx-3" href="board.jsp?page=1">챌린지</a></li>
 					<li class="nav-item"><a class="header_menu nav-link mx-3" href="/recycle_project/jsp/member/regionchallenge.jsp">지도에서 찾기</a></li>
 					<li class="nav-item"><a class="header_menu nav-link mx-3" href="topmember.jsp">랭킹</a></li>
-					<li class="nav-item"><a class="header_menu nav-link mx-3" href="board.jsp?cno=4">공지사항</a></li>
+					<!-- <li class="nav-item"><a class="header_menu nav-link mx-3" href="board.jsp?cno=4">공지사항</a></li> -->
 					
 					<!-- 드롭다운 -->
 					<!-- <li class="nav-item dropdown"><a
